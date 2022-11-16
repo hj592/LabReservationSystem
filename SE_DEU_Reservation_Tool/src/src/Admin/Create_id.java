c1a87c01b6aabdd61081f9ffe38345c35b57b98a
sjh990519 <sjh990519@gmail.com>
2022-11-17 01:37:17 +0900

    MODIFY

--- SE_DEU_Reservation_Tool/src/src/Admin/Create_id.java
+++ SE_DEU_Reservation_Tool/src/src/Admin/Create_id.java
@@ -5,10 +5,30 @@
  */
 package src.Admin;
 
+
+import DB.DB_CONNECTER;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author admin
  */
 public class Create_id {
+    
+        public String getID(String id){
+        String name = "0";
+        try {
+            String[][] pname = DB_CONNECTER.Exe_Qurey("SELECT user_id FROM User WHERE user_id='"+id+"'");            
+            if(pname.length!=1) 
+                name = pname[1][0]; 
+        } catch (SQLException ex) {
+            Logger.getLogger(Create_id.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Create_id.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return name;
+    } 
     
 }
